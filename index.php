b0b42a6af7a329ee56f3e49edfdb3dbf470bc58c
HuriatiPutri <Huriatiputri@gmail.com>
2019-04-10 15:21:06 +0700

    Update index.php

--- index.php
+++ index.php
@@ -48,10 +48,107 @@ $connectionString = "DefaultEndpointsProtocol=https;AccountName=huriatiwebapp;Ac
 $blobClient = BlobRestProxy::createBlobService($connectionString);
 
 ?> 
-form1111
+form111111
 <form action="" method="post" enctype="multipart/form-data">
         <Input type="file" name="file">
         <input type="submit" name="simpan">
     </form>
 
+<?php
+    if(isset($_POST['simpan'])){
 
+$fileToUpload_ = ($_FILES['file']['name']);
+$fileToUpload = file_get_contents($_FILES['file']['tmp_name']);
+//echo $fileToUpload;
+
+if (!isset($_GET["Cleanup"])) {
+    // Create container options object.
+
+    $createContainerOptions = new CreateContainerOptions();
+
+    // Set public access policy. Possible values are
+    // PublicAccessType::CONTAINER_AND_BLOBS and PublicAccessType::BLOBS_ONLY.
+    // CONTAINER_AND_BLOBS:
+    // Specifies full public read access for container and blob data.
+    // proxys can enumerate blobs within the container via anonymous
+    // request, but cannot enumerate containers within the storage account.
+    //
+    // BLOBS_ONLY:
+    // Specifies public read access for blobs. Blob data within this
+    // container can be read via anonymous request, but container data is not
+    // available. proxys cannot enumerate blobs within the container via
+    // anonymous request.
+    // If this value is not specified in the request, container data is
+    // private to the account owner.
+    $createContainerOptions->setPublicAccess(PublicAccessType::CONTAINER_AND_BLOBS);
+
+    // Set container metadata.
+    $createContainerOptions->addMetaData("key1", "value1");
+    $createContainerOptions->addMetaData("key2", "value2");
+
+      $containerName = "blob".generateRandomString();
+
+    try {
+        // Create container.
+        $blobClient->createContainer($containerName, $createContainerOptions);
+
+        // Getting local file so that we can upload it to Azure
+         $myfile = fopen($fileToUpload_, "w") or die("Unable to open file!");
+         fclose($myfile);
+        
+        # Upload file as a block blob
+        echo "Uploading BlockBlob: ".PHP_EOL;
+        echo $fileToUpload_;
+        echo "<br />";
+        
+        $content = fopen($fileToUpload_, "r");
+
+        //Upload blob
+        $blobClient->createBlockBlob($containerName, $fileToUpload_, $content);
+
+        // List blobs.
+        $listBlobsOptions = new ListBlobsOptions();
+        $listBlobsOptions->setPrefix($fileToUpload_);
+
+        echo "These are the blobs present in the container: ";
+
+        do{
+            $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+            foreach ($result->getBlobs() as $blob)
+            {
+                echo $blob->getName().": ".$blob->getUrl()."<br />";
+              //  echo "image : <img src=". $blob->getUrl()."/>";
+            }
+        
+            $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+        } while($result->getContinuationToken());
+        echo "<br />";
+
+        // Get blob.
+       /* echo "This is the content of the blob uploaded: ";
+        $blob = $blobClient->getBlob($containerName, $fileToUpload);
+        fpassthru($blob->getContentStream());
+        echo "<br />";
+*/
+        
+        
+    }
+    catch(ServiceException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
+    catch(InvalidArgumentTypeException $e){
+        // Handle exception based on error codes and messages.
+        // Error codes and messages are here:
+        // http://msdn.microsoft.com/library/azure/dd179439.aspx
+        $code = $e->getCode();
+        $error_message = $e->getMessage();
+        echo $code.": ".$error_message."<br />";
+    }
+} 
+}
+?>
