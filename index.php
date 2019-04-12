5b0c0256ed3eaa940dde2c3fbe38785f3ee688b5
HuriatiPutri <Huriatiputri@gmail.com>
2019-04-12 10:52:44 +0700

    Update index.php

--- index.php
+++ index.php
@@ -174,7 +174,7 @@ if (!isset($_GET["Cleanup"])) {
         };
  
         // Display the image.
-        var sourceImageUrl = "tujuh.jpg";
+        var sourceImageUrl = "https://huriatiapp.azurewebsites.net/tujuh.jpg";
         document.querySelector("#sourceImage").src = sourceImageUrl;
  
         // Make the REST API call.
